c086a5a91f6b1b74c9a6e7551e3813cdeba70655
Elian_Rosales <ros171238@uvg.edu.gt>
2024-06-07 14:47:07 -0600

    Update Prueba.py

--- Prueba.py
+++ Prueba.py
@@ -1,30 +1,41 @@
-#Elián Rosales
+# Elian Rosales
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
+from selenium.webdriver.chrome.service import Service
+import time
 
 driver = webdriver.Chrome()
 
-driver.get("https://automationintesting.com/selenium/testpage/")
+# Guardamos la URL del formulario en una variable para poder acceder a ella más fácilmente 
+form_url = 'https://docs.google.com/forms/d/e/1FAIpQLScz2mqlmoHNqlXgEdd_Q4blqBSA73-Z7HMQpDhQDo1KmSJrNg/viewform'
+driver.get(form_url)
 
+#Nombre
+name_field = driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input')
+name_field.send_keys('Elián')
 
-name_field = driver.find_element(By.NAME, "firstname")
-name_field.send_keys("John")
+# Apelido
+last_name_field = driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input')
+last_name_field.send_keys('Rosales')
 
-name_field = driver.find_element(By.NAME, "surname")
-name_field.send_keys("Doe")
+# Edad
+age_field = driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input')
+age_field.send_keys('20')
 
-dropdown_element = driver.find_element_by_id("gender")
-dropdown = Select(dropdown_element)
-dropdown.select_by_visible_text("Male")
+# Correo
+email_field = driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[4]/div/div/div[2]/div/div[1]/div/div[1]/input')
+email_field.send_keys('elianrosales854@gmail.com')
 
-radio_button = driver.find_element_by_id("red")
-if not radio_button.is_selected():
-    radio_button.click()
+# Fecha
+date_field = driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[5]/div/div/div[2]/div/div/div[2]/div[1]/div/div[1]/input')
+date_field.send_keys('06/06/2024')
 
-submit_button = driver.find_element(By.XPATH, "//input[@type='submit']")
+# Enviar
+submit_button = driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div[1]/div/span/span')
 submit_button.click()
 
-driver.quit()
+time.sleep(3)
 
+driver.quit()
